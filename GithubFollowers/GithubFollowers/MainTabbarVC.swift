6fe31f58a1ee86ee63b15ce070a7b78877eba4d4
Mehmet Ali Özdemir <mehmetaliozdemir@Mehmets-MacBook-Air.local>
2023-12-24 14:13:28 +0300

    chore: init app

---
+++ GithubFollowers/GithubFollowers/MainTabbarVC.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabbarVC.swift
+//  GithubFollowers
+//
+//  Created by Mehmet Ali Özdemir on 24.12.2023.
+//
+
+import UIKit
+
+final class MainTabbarVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
---
+++ GithubFollowers/GithubFollowers/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  GithubFollowers
+//
+//  Created by Mehmet Ali Özdemir on 24.12.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        let vc = MainTabbarVC()
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible()
+    }
+}
+
