6594b879453c4196f0c2bb65bf06c2354ddd88fc
slash585 <slsh585@gmail.com>
2024-04-14 13:37:00 +0300

    Init app

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Mehmet Ali Özdemir on 14.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
